aa03b9dd7cf38b7a7cab64fffaa1fe14ae89fd55
Eric Stimmel <estimmel@gmail.com>
2024-11-14 16:34:26 -0800

    Update HelloWorld.cs

--- Standards/RevitStandardsPanel/AnnotatedTest/HelloWorld.cs
+++ Standards/RevitStandardsPanel/AnnotatedTest/HelloWorld.cs
@@ -7,10 +7,15 @@ using Autodesk.Revit.UI;
 // change the name of the class to match the name of the cs file
 public class HelloWorld
 {
-	TaskDialog mainDialog = new TaskDialog.Show("Custom Rule");
-	mainDialog.MainInstruction = "Hello World!";
-	mainDialog.MainContent = "This sample shows how to use a Revit task dialog to communicate with the user.";
-	// return the ids of elements that failed the rule, or <null>
-	mainDialog.Show();
-	return null;
+	// doc = active document
+	// ids = the added or changed elements, or <null> if running the rule on the entire document
+	public IEnumerable<ElementId> Run(Document doc, List<ElementId> ids)
+	{
+		TaskDialog mainDialog = new TaskDialog.Show("Custom Rule");
+		mainDialog.MainInstruction = "Hello World!";
+		mainDialog.MainContent = "This sample shows how to use a Revit task dialog to communicate with the user.";
+		// return the ids of elements that failed the rule, or <null>
+		mainDialog.Show();
+		return null;
+	}
 }
