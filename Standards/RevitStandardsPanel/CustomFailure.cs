b13a4c86f48bb811de05068138c149368c6713a0
T-Lane-00 <trey.lane@hok.com>
2025-09-04 13:20:58 -0500

    Add files via upload
    add custom failures message for pre-loading of error messages

---
+++ Standards/RevitStandardsPanel/CustomFailure.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace RevitDataValidator
+{
+    public class CustomFailureWrapper
+    {
+        public List<CustomFailure> CustomFailures { get; set; }
+    }
+    public class CustomFailure
+    {
+        public Guid FailureGuid { get; set; }
+        public string Message { get; set; }
+    }
+}
